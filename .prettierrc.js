69af22ddfed0e57977e6d28fc6686292c28707dd
iamrishupatel <rishupatel04@gmail.com>
2023-09-16 14:52:08 +0530

    feat: add editor config and minor refacotrs

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,7 @@
 module.exports = {
-  semi: true, // Use semicolons at the end of statements
-  trailingComma: 'all', // Use trailing commas wherever possible (multiline arrays, objects, etc.)
-  singleQuote: true, // Use single quotes for strings
-  printWidth: 100, // Maximum line width before wrapping
-  tabWidth: 2, // Number of spaces for each indentation level
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 72,
+  tabWidth: 2,
 };
--- src/constants/routes.constants.ts
+++ src/constants/routes.constants.ts
@@ -1,3 +1,3 @@
 export const ROUTES = Object.freeze({
-  HOME: "/",
+  HOME: '/',
 });
