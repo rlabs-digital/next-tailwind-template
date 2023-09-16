7afccc386da040641932be3d7f4140135d474410
iamrishupatel <rishupatel04@gmail.com>
2023-09-16 16:00:39 +0530

    fix: linting issue in lint-staged

--- next.config.js
+++ next.config.js
@@ -1,50 +1,39 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer');
 const withPlugins = require('next-compose-plugins');
-
-/** @type {import('next').NextConfig} */
-module.exports = {};
+// /require('dotenv');
 
 /**
  * @type {import('next').NextConfig}
  */
-const config = withPlugins([[withBundleAnalyzer({ enabled: process.env.ANALYZE === 'true' })]], {
-  reactStrictMode: true,
-  experimental: { instrumentationHook: true },
-  trailingSlash: true,
+const config = withPlugins(
+  [[withBundleAnalyzer({ enabled: process.env.NEXT_PUBLIC_ANALYZE === 'true' })]],
+  {
+    reactStrictMode: true,
+    trailingSlash: true,
 
-  // Add custom webpack configurations if needed
-  webpack(config, { isServer }) {
-    if (isServer) {
-      // Server-side webpack configuration
-    } else {
-      // Client-side webpack configuration
-    }
-
-    return config;
+    // Add your custom headers for enhanced security
+    async headers() {
+      return [
+        {
+          source: '/(.*)',
+          headers: [
+            {
+              key: 'X-Content-Type-Options',
+              value: 'nosniff',
+            },
+            {
+              key: 'X-Frame-Options',
+              value: 'SAMEORIGIN',
+            },
+            {
+              key: 'X-XSS-Protection',
+              value: '1; mode=block',
+            },
+          ],
+        },
+      ];
+    },
   },
-
-  // Add your custom headers for enhanced security
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-          {
-            key: 'X-Frame-Options',
-            value: 'SAMEORIGIN',
-          },
-          {
-            key: 'X-XSS-Protection',
-            value: '1; mode=block',
-          },
-        ],
-      },
-    ];
-  },
-});
+);
 
 module.exports = config;
